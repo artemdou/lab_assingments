c118c57f3670310a406c59ad2c5244a7dc89ea13
artemdou <44100345+artemdou@users.noreply.github.com>
2020-04-01 10:11:01 +0300

    add JavaDoc

--- seip2020_practical_assignments/gradeshistogram/src/main/java/gradeshistogram/HistogramGenerator.java
+++ seip2020_practical_assignments/gradeshistogram/src/main/java/gradeshistogram/HistogramGenerator.java
@@ -1,6 +1,7 @@
 package gradeshistogram;
 
 import java.io.BufferedReader;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -13,6 +14,14 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
+
+/***
+ * 
+ * @author artemdou 
+ * The purpose of this class is to produce a histogram of the frequency of an array of grades
+ * (ranged from 0 to 10) using a JFreeChart XYLine chart.
+ * @since March 2020 
+ */
 
 public class HistogramGenerator {
 
@@ -32,46 +41,14 @@ public class HistogramGenerator {
 		demo.generateChart(grades);
     }
 	
-/*	public static Integer[] getFrequencies(Integer[] grades) {
-		Integer[] frequencies = {0,0,0,0,0,0,0,0,0,0,0};
-		for (int i = 0; i < grades.length;){
-			if (grades[i] == 0) {
-				frequencies[0]++;
-			}
-			if (grades[i] == 1) {
-				frequencies[1]++;
-			}
-			if (grades[i] == 2) {
-				frequencies[2]++;
-			}
-			if (grades[i] == 3) {
-				frequencies[3]++;
-			}
-			if (grades[i] == 4) {
-				frequencies[4]++;
-			}
-			if (grades[i] == 5) {
-				frequencies[5]++;
-			}
-			if (grades[i] == 6) {
-				frequencies[6]++;
-			}
-			if (grades[i] == 7) {
-				frequencies[7]++;
-			}
-			if (grades[i] == 8) {
-				frequencies[8]++;
-			}
-			if (grades[i] == 9) {
-				frequencies[9]++;
-			}
-			if (grades[i] == 10) {
-				frequencies[10]++;
-			}
-		}
-		return frequencies;
-	} */
-	
+	/**
+	 * Receives as input an object type File, which represents a .txt file that contains
+	 * one column of integers, one in every line of the file. It returns an arraylist of 
+	 * the integers/lines of the file.
+	 * 
+	 * @param file
+	 * @return
+	 */
 	public static ArrayList<Integer> readLines(File file) {
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(file));
@@ -93,24 +70,25 @@ public class HistogramGenerator {
 		} finally {
 		}
 	}
-	
+	/**
+	 * Receives an Integer ArrayList as input and returns an array with the same
+	 * content as the ArrayList.
+	 * @param li
+	 * @return
+	 */
 	public static Integer[] arrayListToArray(ArrayList<Integer> li) {
 		Integer[] array = li.toArray(new Integer[li.size()]);
 		return array;
 	}
 	
-	public void generateChart(Integer[] dataValues) {
-		/*
-		 * The XYSeriesCollection object is a set XYSeries series (dataset) that
-		 * can be visualized in the same chart
-		 */
-		XYSeriesCollection dataset = new XYSeriesCollection();
-		/*
-		 * The XYSeries that are loaded in the dataset. There might be many
-		 * series in one dataset.
-		 */
-		XYSeries data = new XYSeries("random values");
-
+	/***
+	 * Receives a single dimension Integer array. This array represents data that range between 
+	 * 0-10. It returns an array with length 11 that contains the frequency each 'number' (0-10)
+	 * appears in the input array.
+	 * 
+	 * @param dataValues Single dimension integer array
+	 */
+	public static Integer[] getFrequencies(Integer[] dataValues) {
 		/*
 		 * Get the Integer array of the frequency of the dataValues values
 		 */
@@ -150,11 +128,34 @@ public class HistogramGenerator {
 					frequencies[10]++;
 				}
 			}
+		return frequencies;
+	}
+	
+	/***
+	 * Receives a single dimension Integer array. From this array the dataset
+	 * that will be used for the visualization is generated. Finally, The chart
+	 * is generated with the use of the aforementioned dataset and then
+	 * presented in the screen.
+	 * 
+	 * @param dataValues Single dimension integer array
+	 */
+	public void generateChart(Integer[] dataValues) {
+		/*
+		 * The XYSeriesCollection object is a set XYSeries series (dataset) that
+		 * can be visualized in the same chart
+		 */
+		XYSeriesCollection dataset = new XYSeriesCollection();
+		/*
+		 * The XYSeries that are loaded in the dataset. There might be many
+		 * series in one dataset.
+		 */
+		XYSeries data = new XYSeries("random values");
 		
 		/*
 		 * Populating the XYSeries data object from the input Integer array
 		 * values.
 		 */
+		Integer[] frequencies = getFrequencies(dataValues);
 		for (int i = 0; i < 11; i++) {
 			data.add(i, frequencies[i]);
 		}
@@ -179,7 +180,5 @@ public class HistogramGenerator {
 		// makes the previously created frame visible
 		frame.setVisible(true);
 	}
-
-	 
 
 }
