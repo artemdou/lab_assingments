7d0f807e59586fd3a0db49465a15297a8136de07
artemdou <44100345+artemdou@users.noreply.github.com>
2020-04-19 22:47:41 +0300

    add tests for divide method

--- seip2020_practical_assignments/math/src/test/java/math/ArithmeticOperationsTest.java
+++ seip2020_practical_assignments/math/src/test/java/math/ArithmeticOperationsTest.java
@@ -1,5 +1,38 @@
 package math;
 
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import math.ArithmeticOperations;
+
 public class ArithmeticOperationsTest {
 
+	ArithmeticOperations op; 
+	
+	/*
+	 * A test case that examines the divide method
+	 * with 'normal' input values. 
+	 */
+	@Test
+	public void testDevideNormal() {
+		Assert.assertEquals(2, op.divide(4, 2), 0);
+	}
+	/*
+	 * A test case that examines the divide method
+	 * with numerator equal to zero. 
+	 */
+	@Test
+	public void testDevideZeroNum() {
+		Assert.assertEquals(0, op.divide(0, 100), 0);
+	}
+	
+	@Test (expected = ArithmeticException.class)
+	public void testDevideZeroDen() {
+		op.divide(1, 0);
+	}
+	
+	
+	
+	
 }
