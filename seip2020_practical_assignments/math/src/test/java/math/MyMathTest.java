a85b4990709044c3284e4a545ede014ef493cb7c
artemdou <44100345+artemdou@users.noreply.github.com>
2020-04-20 02:05:30 +0300

    add test cases for MyMath factorial

---
+++ seip2020_practical_assignments/math/src/test/java/math/MyMathTest.java
@@ -0,0 +1,36 @@
+package math;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import math.MyMath;
+
+public class MyMathTest {
+
+	MyMath mm;
+	
+	public MyMathTest() {
+		this.mm = new MyMath();
+	}
+	
+	@Rule 
+	public ExpectedException thrown = ExpectedException.none(); 
+
+	@Test 
+	public void testFactorialInvalidInput() {
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Invalid input");
+		mm.factorial(-5);
+	}
+	
+	@Test
+	public void testFactorialNormal() {
+		Assert.assertEquals(720, mm.factorial(6), 0);
+	}
+	
+	@Test
+	public void testFactorialZero() {
+		Assert.assertEquals(1, mm.factorial(0), 0);
+	}
+}
