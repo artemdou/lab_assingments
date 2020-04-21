827d7788270ddba3d4a76feaee80fea116018edb
artemdou <44100345+artemdou@users.noreply.github.com>
2020-04-21 19:58:28 +0300

    add NumberFormatException in javadoc

--- seip2020_practical_assignments/math/src/main/java/io/FileIO.java
+++ seip2020_practical_assignments/math/src/main/java/io/FileIO.java
@@ -26,6 +26,7 @@ public class FileIO {
 	 * @exception IllegalArgumentException when the given file does not exist
 	 * @exception IllegalArgumentException when the given file is empty
 	 * @exception IOException when an IO interruption occurs (not required to be tested)
+	 * @exception NumberFormatException when a non-int value tries to be parsed
 	 */
 	public int[] readFile(String filepath) {
 		File file = new File(filepath);
